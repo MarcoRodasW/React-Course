8cc014439acb79093f1c80989015251c4c816f18
Marco Rodas <mrodasw@gmail.com>
2022-10-18 23:07:52 -0500

    Creation of the first component

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from "./components/Counter"
+
+export default function App () {
+
+
+  
+  return (
+    <>
+    <div className="flex items-center justify-center mt-4">
+      <h1 className="text-slate-800 text-[24px] font-bold font">This is the first lesson of the React Course</h1>
+    </div>
+    <Counter />
+    </>
+  )
+}
+
---
+++ counter-app/src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex gap-2 justify-center items-center mt-8 ">
+        <button
+          className="bg-slate-600 text-gray-300 p-2 rounded-xl"
+          onClick={() => setCount((count)=> count+1)}
+        >
+          Increase
+        </button>
+        <p className="text-lg text-slate-800 font-bold">{count}</p>
+        <button
+          className="bg-slate-600 text-gray-300 p-2 rounded-xl"
+          onClick={() => setCount((count) => count - 1)}
+        >
+          Decrease
+        </button>
+      </div>
+      <div className="flex justify-center items-center mt-5">
+        <button
+          className="bg-red-600 text-slate-800 p-2 rounded-xl"
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
+      </div>
+    </>
+  );
+}
