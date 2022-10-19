022e033beba581d8b8f599dd008f7987402e20a6
Marco Rodas <mrodasw@gmail.com>
2022-10-18 23:09:45 -0500

    Header changed

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -7,7 +7,7 @@ export default function App () {
   return (
     <>
     <div className="flex items-center justify-center mt-4">
-      <h1 className="text-slate-800 text-[24px] font-bold font">This is the first lesson of the React Course</h1>
+      <h1 className="text-slate-800 text-[24px] font-bold font">This is the first lesson of the React Course!!</h1>
     </div>
     <Counter />
     </>
