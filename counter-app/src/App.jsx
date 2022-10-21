a8f14a95c2380d1a9f6a6a145269bc490db99c74
Marco Rodas <mrodasw@gmail.com>
2022-10-20 23:59:15 -0500

    Setup Eslint and prettier

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,16 +1,14 @@
-import Counter from "./components/Counter"
-
-export default function App () {
-
-
-  
+import { Counter } from "./components/Counter";
+export default function App() {
+  const title = "Library";
   return (
     <>
-    <div className="flex items-center justify-center mt-4">
-      <h1 className="text-slate-800 text-[24px] font-bold font">This is the first lesson of the React Course!!</h1>
-    </div>
-    <Counter />
+      <div className="flex items-center justify-center mt-4">
+        <h1 className="text-slate-800 text-[24px] font-bold font">
+          This is the first lesson of the React {title}!!
+        </h1>
+      </div>
+      <Counter />
     </>
-  )
+  );
 }
-
