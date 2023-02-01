29e5e6e8119f1f8d80ea607260e70e8398cae7b4
MarcoRodasW <mrodasw@gmail.com>
2023-01-31 23:41:33 -0600

    updated version of counter app

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,14 +1,24 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 
 export function Counter() {
   const [count, setCount] = useState(0)
 
+  const handleIncrease = () => {
+    setCount((count) => count + 1)
+  }
+
+  useEffect(() => {
+    if (count > 10 || count < -10) {
+      alert('Limited amount')
+      setCount(0)
+    }
+  }, [count])
+
   return (
     <>
       <div className="flex gap-2 justify-center items-center mt-8 ">
-        <button
-          className="bg-slate-600 text-gray-300 p-2 rounded-xl"
-          onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-slate-600 text-gray-300 p-2 rounded-xl" onClick={handleIncrease}>
           Increase
         </button>
         <p className="text-lg text-slate-800 font-bold">{count}</p>
--- counter-app/src/components/Navbar.jsx
+++ counter-app/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from './Link'
 export const Navbar = () => {
   return (
     <>
-      <nav className="bg-gray-800">
+      <nav className="bg-gray-800 ">
         <div className="container mx-auto h-16 ">
           <div className="flex justify-center items-center h-full">
             <div className="space-x-4">
-              <Link name={'Dashboard'} goTo="www.google.hn" />
+              <Link name={'Dashboard'} goTo="https://www.google.hn" />
               <Link name={'About Me'} goTo="#" />
               <Link name={'Projects'} goTo="#" />
               <Link name={'Gallery'} goTo="#" />
